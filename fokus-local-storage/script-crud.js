ed66e8a9765a816a8dfce4e149d42f2d721005b0
EduardoL0pes <eduardo_lopes010@hotmail.com>
2023-12-03 17:12:38 -0300

    criando array de tarefas e salvando em localStorage

--- fokus-local-storage/script-crud.js
+++ fokus-local-storage/script-crud.js
@@ -1,6 +1,18 @@
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task');
 const adicionarTarefa = document.querySelector('.app__form-add-task');
+const textarea = document.querySelector('.app__form-textarea');
+
+const tarefas = [];
 
 btnAdicionarTarefa.addEventListener('click', () => {
     adicionarTarefa.classList.toggle('hidden');
+});
+
+adicionarTarefa.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+    const tarefa = {
+        descrição: tarefas.value
+    };
+    tarefas.push(tarefa)
+    localStorage.setItem('tarefas', tarefas);
 });
